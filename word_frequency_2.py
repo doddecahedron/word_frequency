01d60dc83fdb1668dd0d53a10541084dbe710877
Doddecahedron <mpdodd7@gmail.com>
2016-02-05 10:28:25 -0500

    Find the word frequency in a text file

---
+++ word_frequency_2.py
@@ -0,0 +1,18 @@
+file = open("sample.txt", "r")
+text = file.read().lower().replace("\n", " ").replace(".", " ").replace("?", " ").replace("-", " ").replace(",", " ").replace(":", " ").replace("!", " ").replace("--", " ").replace('"', " ").replace(";", " ").split(' ')
+file.close()
+word_list = text
+word_freq = {}
+
+for line in text:
+    words = line.split()
+    for word in words:
+        k = word.lower()
+        if "'" in k:
+            ks = k.split("'")
+        else:
+            ks = [k, ]
+        for k in ks:
+            word_freq[k] = word_freq.get(k, 0) + 1
+for word in sorted(word_freq, key=lambda k: word_freq[k], reverse=True)[:10]:
+    print(word, "\t", word_freq[word])
